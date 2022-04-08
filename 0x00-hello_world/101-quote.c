80d9ce75f7e54b67abf56940e3e23b898f349ec3
fortune <fortyoun@gmail.com>
2022-04-07 18:47:25 -0700

    loll

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <unistd.h>
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
-	return(1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
 }
