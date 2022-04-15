7e91c748eb3ed3e512bb9684220c61c360763541
fortune <fortyoun@gmail.com>
2022-04-15 10:28:12 -0700

    jg

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
-
 /**
- *main - tests func
- *Return: 0
+ * main - main
+ * Return: 0
  */
 
 int main(void)
 {
-	int i;
+int i;
+i = 0;
 
-        i = 0;
-        positive_or_negative(i);
+positive_or_negative(i);
 
-        return (0);
+return (0);
+
 }
