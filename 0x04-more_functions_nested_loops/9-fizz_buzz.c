202bfc8b124bf3587c945e8a001051aa1a59cd6d
fortune <fortyoun@gmail.com>
2022-04-15 19:11:56 -0700

    for the nig

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - entry point
- *
- * Return: void
+ * main - print 1 to 100,
+ * multiples of 3 with Fizz,
+ * multiples of 5 with Buzz
+ * and multiple of both 3 and 5 with FizzBuzz
+ * Return: 0 on success
  */
 
 int main(void)
@@ -11,28 +14,18 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else if (i % 3 == 0 && i % 5 == 0)
-		{
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
-		}
-		else 
-		{
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
 			printf("%d", i);
-		}
-		
-		if (i <100)
-		{
-			print(" ");
+
+		if (i < 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
 }
-
