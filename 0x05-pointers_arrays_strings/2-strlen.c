d277d9d0f155007fda96ea9c922a724912750d9e
fortune <fortyoun@gmail.com>
2022-04-20 15:25:28 -0700

    heyy

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  *_strlen -  returns the length of a string.
+  *@s: S pointer that will be updated 
+  *Return: 0
+  */
+
+int _strlen(char *s)
+{
+	int k = 0;
+
+	while (s[k] != '\0')
+		k++;
+	return (k);
+}
