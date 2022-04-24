4ff2c3708b8ae34c660428673d0551641e9ab385
fortune <fortyoun@gmail.com>
2022-04-24 11:22:01 -0700

    riot

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/*_strcat - concatenate two strings
+ *@dest: string to be appened 
+ *@src: sorce
+ *Return: returns dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
