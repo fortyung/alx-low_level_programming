87e0ec2cef5752921e8be6160176440c03fb2fe7
fortune <fortyoun@gmail.com>
2022-05-15 19:15:01 -0700

    2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,7 +18,7 @@ int int_index(int *array, int size, int (*cmp)(int a))
 		return (-1);
 
 	for(i = 0; i < size; i++)
-		if (cmp(array[i]))
+		if (cmp(*(array + i)))
 			return (i);
 	return (-1);
 }
