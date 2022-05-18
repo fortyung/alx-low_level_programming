8b011c62993046c11bbc3197be8d211aa187cd05
fortune <fortyoun@gmail.com>
2022-05-17 17:52:09 -0700

    kgg

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,16 +9,18 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
+	va_list valist;
 	unsigned int sum = 0, i;
 
+	/* validate valist and initialize */
 	if (n == 0)
 		return (0);
-	va_start(ap, n);
+	va_start(valist, n);
 
+	/* iterate through list, update sum, free list */
 	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
-	va_end(ap);
+		sum += va_arg(valist, int);
+	va_end(valist);
 
 	return (sum);
 }
