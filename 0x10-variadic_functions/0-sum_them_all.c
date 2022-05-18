717857c46e7afb218ca098ccab6a43d681183b42
fortune <fortyoun@gmail.com>
2022-05-17 17:40:56 -0700

    one

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,7 @@
 
 /**
   *sum_them_all - function that sums all parameters
-  *@n: no. of intergers to add
+  *@n: num of intergers to add
   *Return: sum
   */
 
@@ -14,10 +14,13 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n == 0)
 		return (0);
+
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
+	{
 		sum += va_arg(ap, int);
+	}
 
 	va_end(ap);
 	return (sum);
